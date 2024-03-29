0983095aacd1618c2f190fc1d37c77c5933023b0
Juan Patten <jrp@maild.name>
2024-03-29 14:23:18 -0700

    Fix cycle detection and do some tweaks for easier debugging

--- urx-part1.js
+++ urx-part1.js
@@ -11,12 +11,10 @@ export function Atom(value) {
     r.latest = newValue
     r.fresh()
   }
-  atom.alter = (transform) => {
-    atom.set(transform(r.latest))
-  }
   atom.peek = () => r.latest
   atom.dispose = r.dispose.bind(r)
-  atom.label = (lbl) => ((r.__label = lbl), atom)
+  atom.label = (lbl) => ((r._label = lbl), atom)
+  atom.$r = r
   return atom
 }
 
@@ -27,7 +25,8 @@ export function Calc(fn) {
   const calc = () => r.observe()
   calc.peek = () => r.latest
   calc.dispose = r.dispose.bind(r)
-  calc.label = (lbl) => ((r.__label = lbl), calc)
+  calc.label = (lbl) => ((r._label = lbl), calc)
+  calc.$r = r
   return calc
 }
 
@@ -40,72 +39,94 @@ export function Effect(action) {
 // Implementation
 
 export class Reactor {
-  static running = null
+  static active = null
+
+  _label = undefined
+
+  _inputs = new Set()
+  _outputs = new Set()
+
+  _staleInputs = 0
+  _inputChanged = false
+  _becomingStale = false
 
   latest = undefined
   effect = undefined
 
-  #staleInputs = 0
-  #changedInputs = 0
-  #inputs = new Set()
-  #outputs = new Set()
-
-  // Informs the currently-running reactor that this
-  // is one of its inputs. Returns the latest value.
   observe() {
-    let running = Reactor.running
-    if (running) {
-      this.#outputs.add(running)
-      running.#inputs.add(this)
+    let active = Reactor.active
+    if (active) {
+      this._outputs.add(active)
+      active._inputs.add(this)
+      if ((active == this) || (this._staleInputs > 0)) {
+        throw Error(`Cycle detected in ${active._label ?? '?'}`)
+      }
     }
     return this.latest
   }
 
-  // Records a stale input. If not already stale,
-  // becomes stale and notifies its outputs.
   stale() {
-    if (++this.#staleInputs == 1) {
-      for (const o of this.#outputs) { o.stale() }
+    if (this._becomingStale) { return /* cycle */ }
+    if (++this._staleInputs == 1) {
+      this._becomingStale = true
+      for (const o of this._outputs) { o.stale() }
+      this._becomingStale = false
     }
   }
 
-  // Records a fresh input. If all are fresh,
-  // runs effect (if any) and notifies outputs.
-  fresh(didChange = true) {
-    if (didChange) { ++this.#changedInputs }
-    if (--this.#staleInputs == 0) {
-      if ((this.#changedInputs > 0) && (this.effect != null)) {
+  fresh(changed = true) {
+    if (this._staleInputs == 0) { return /* cycle */ }
+    if (changed) { this._inputChanged = true }
+    if (--this._staleInputs == 0) {
+      if (this._inputChanged && (this.effect != null)) {
         let oldValue = this.latest
-        this.effect?.()
-        didChange = (this.latest !== oldValue)
-        this.#changedInputs = 0
+        this.effect()
+        changed = (this.latest !== oldValue)
       }
-      for (const o of this.#outputs) { o.fresh(didChange) }
+      this._inputChanged = false
+      for (const o of this._outputs) { o.fresh(changed) }
     }
   }
 
-  // Executs `fn` and records its inputs.
   track(fn) {
-    const oldInputs = this.#inputs
-    const oldRunning = Reactor.running
-    this.#inputs = new Set()
-    Reactor.running = this
+    const oldInputs = this._inputs
+    this._inputs = new Set()
+    const oldActive = Reactor.active
+    Reactor.active = this
     try {
       return fn()
+    } catch(err) {
+      return err
     } finally {
-      Reactor.running = oldRunning
-      for (const i of oldInputs.difference(this.#inputs)) {
-        i.#outputs.delete(this)
+      Reactor.active = oldActive
+      for (const i of oldInputs.difference(this._inputs)) {
+        i._outputs.delete(this)
       }
     }
   }
 
-  // Removes this reactor from the dependency graph
   dispose() {
-    for (const i of this.#inputs) { i.#outputs.delete(this) }
-    for (const o of this.#outputs) { o.#inputs.delete(this) }
-    this.#inputs.clear()
-    this.#outputs.clear()
+    for (const i of this._inputs) { i._outputs.delete(this) }
+    for (const o of this._outputs) { o._inputs.delete(this) }
+    this._inputs.clear()
+    this._outputs.clear()
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Shims
+
+if (Set.prototype.difference == null) {
+  Set.prototype.difference = function(other) {
+    const diff = new Set()
+    if (this.size > other.size) {
+      for (let item of other) if (!this.has(item)) diff.add(item);
+    } else {
+      for (let item of this) if (!other.has(item)) diff.add(item);
+    }
+    return diff
   }
 }
 
